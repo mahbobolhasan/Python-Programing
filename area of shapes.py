51233cbfcebc30d2770694876c2dd3cce1401254
mahbobolhasan <m.h.santho1999@gmail.com>
2025-08-31 14:36:11 +0600

    Add files via upload

---
+++ area of shapes.py
@@ -0,0 +1,16 @@
+#Area of Square
+a= int (input("Enter the value of a ="))
+Square= a*a
+print("Shape of Square is=", Square)
+
+#Area of Circle
+r= int (input("Enther the value of Radius="))
+Circle = 3.1516*r*r
+print("Area of Circle is =", Circle)
+
+#Area of Triangle
+b = int (input("Enther Base="))
+h = int (input("Enter Height="))
+Triangle = 1/2*b*h
+print("Triangle is =", Triangle)
+
