ec56919abccb9d06d8b94e5489233c18fe02245d
mahbobolhasan <m.h.santho1999@gmail.com>
2025-08-31 14:32:11 +0600

    Use of basi If else statements

---
+++ if_else.py
@@ -0,0 +1,9 @@
+GPA= int(input("Enter your GPA="))
+if GPA >=80:
+    print("Congratulations, You got A+")
+elif GPA >=60:
+    print("Congrats, You got A")
+elif GPA >=50:
+    print("Congrats, You got B")
+else:
+    print("Sorry, You are failed")
