c3beec1565616d4764a5f72e443a4ee0f512fd32
mahbobolhasan <m.h.santho1999@gmail.com>
2025-08-31 14:36:41 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,15 @@
+a = int (input ("Enter the value of a ="))
+b = int (input ("Enter the value of b="))
+
+''''
+add = int (a)+ int (b)
+print("Addition is =", add)
+sub = int (a) - int (b)
+print("Subtraction is =", sub)
+mul = int (a) * int (b)
+print("Multiplication is =", mul)
+div = int (a) /int (b)
+print ("Division is =", div)
+'''
+add= a+b
+print ("Addition is=", add)
