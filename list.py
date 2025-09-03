e81587b1b00a6b365e41c6e30b1ec760df4886de
mahbobolhasan <m.h.santho1999@gmail.com>
2025-09-03 19:14:35 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+name = ["Mahbobol","Hasan","Shanto","Shohag","Karim","Rahim"]
+print(name) #Whole variable
+print(name[2]) #Only the 2nd variable start from 0.
+print(name[1:]) #From 1 to last variables.
+print(name[-1]) #Reverse
+print("Shanto" in name) #Checking the variable either exist or not.
+print("Stain" in name)
+print("Lee" not in name) #For checking not existing.
+print(name +["Robinson", 420]) #Add
